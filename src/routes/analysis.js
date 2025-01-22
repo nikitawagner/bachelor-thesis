c752e65b589b76b35663385e7a148caf42cf51b2
Nikita Wagner <nikita.leon.wagner@gmail.com>
2025-01-22 11:29:36 +0100

    faet: add get sentimental data endpoints

--- src/routes/analysis.js
+++ src/routes/analysis.js
@@ -1,17 +1,91 @@
 import express from "express";
-import { handlePostSingleSentimentRequest } from "../services/handleAnalysisRequests.js";
+import {
+	handleGetAllSentimentalAnalysisRequest,
+	handleGetSentimentAnalysisRequest,
+	handlePostSingleSentimentRequest,
+} from "../services/handleAnalysisRequests.js";
 
 const analysisRouter = express.Router();
 
-analysisRouter.post("/sentiment/all/:date", async (req, res, next) => {
+analysisRouter.get("/sentiment/all/:actionType", async (req, res, next) => {
 	try {
-		const { date } = req.params;
-		const response = await handlePostSingleSentimentRequest(ticker, date);
+		const { actionType } = req.params;
+		const response = await handleGetAllSentimentalAnalysisRequest(
+			actionType.toLowerCase()
+		);
 		res.json({ message: "Success", response });
 	} catch (error) {
 		next(error);
 	}
 });
+
+analysisRouter.get(
+	"/sentiment/all/:actionType/:dateStart/:dateEnd",
+	async (req, res, next) => {
+		try {
+			const { actionType, dateStart, dateEnd } = req.params;
+			const response = await handleGetAllSentimentalAnalysisRequest(
+				actionType,
+				dateStart,
+				dateEnd
+			);
+			res.json({ message: "Success", response });
+		} catch (error) {
+			next(error);
+		}
+	}
+);
+
+// analysisRouter.post("/sentiment/all/:date", async (req, res, next) => {
+// 	try {
+// 		const { date } = req.params;
+// 		const response = await handlePostSingleSentimentRequest(ticker, date);
+// 		res.json({ message: "Success", response });
+// 	} catch (error) {
+// 		next(error);
+// 	}
+// });
+
+analysisRouter.get("/sentiment/:ticker/:actionType", async (req, res, next) => {
+	try {
+		const { ticker, actionType } = req.params;
+		const response = await handleGetSentimentAnalysisRequest(
+			ticker,
+			actionType.toLowerCase()
+		);
+		res.json({ message: "Success", response });
+	} catch (error) {
+		next(error);
+	}
+});
+
+analysisRouter.get(
+	"/sentiment/:ticker/:actionType/:dateStart/:dateEnd",
+	async (req, res, next) => {
+		try {
+			const { ticker, dateStart, dateEnd, actionType } = req.params;
+			const response = await handleGetSentimentAnalysisRequest(
+				ticker,
+				actionType.toLowerCase(),
+				dateStart,
+				dateEnd
+			);
+			res.json({ message: "Success", response });
+		} catch (error) {
+			next(error);
+		}
+	}
+);
+
+// analysisRouter.post("/sentiment/all/:date", async (req, res, next) => {
+// 	try {
+// 		const { ticker, date } = req.params;
+// 		const response = await handlePostSingleSentimentRequest(ticker, date);
+// 		res.json({ message: "Success", response });
+// 	} catch (error) {
+// 		next(error);
+// 	}
+// });
 
 analysisRouter.post("/sentiment/:ticker/:date", async (req, res, next) => {
 	try {
--- src/services/handleAnalysisRequests.js
+++ src/services/handleAnalysisRequests.js
@@ -117,6 +117,95 @@ export const handleGetSentimentAnalysisRequest = async (
 	}
 };
 
+export const handleGetAllSentimentalAnalysisRequest = async (
+	actionType,
+	dateStart,
+	dateEnd
+) => {
+	try {
+		if (dateStart && dateEnd && dateStart !== dateEnd) {
+			const response = await query(
+				`SELECT 
+				json_build_object(
+					'reasons_array', COALESCE(reasons.reasons_array, '[]'::json),
+					'confidence_score', ROUND(d.confidence_score::numeric, 2),
+					'reasoning_summary', d.reasoning_summary,
+					'action', a.action_type
+				) AS result
+			FROM actions a
+			JOIN prices p ON a.fk_price = p.id
+			JOIN decisions d ON a.fk_decision = d.id
+			LEFT JOIN LATERAL (
+				SELECT json_agg(json_build_object(
+					'id', dsd.fk_sentiment_data::text,
+					'reasoning', dsd.reasoning
+				)) AS reasons_array
+				FROM decisions_sentiment_data dsd
+				WHERE dsd.fk_decision = d.id
+			) reasons ON true
+			WHERE a.action_type = $1
+				AND a.datetime BETWEEN $2 AND $3
+			ORDER BY a.datetime DESC;`,
+				[actionType, dateStart, dateEnd]
+			);
+			return response.rows;
+		} else if (dateStart && dateEnd && dateStart === dateEnd) {
+			const response = await query(
+				`SELECT 
+				json_build_object(
+					'reasons_array', COALESCE(reasons.reasons_array, '[]'::json),
+					'confidence_score', ROUND(d.confidence_score::numeric, 2),
+					'reasoning_summary', d.reasoning_summary,
+					'action', a.action_type
+				) AS result
+			FROM actions a
+			JOIN prices p ON a.fk_price = p.id
+			JOIN decisions d ON a.fk_decision = d.id
+			LEFT JOIN LATERAL (
+				SELECT json_agg(json_build_object(
+					'id', dsd.fk_sentiment_data::text,
+					'reasoning', dsd.reasoning
+				)) AS reasons_array
+				FROM decisions_sentiment_data dsd
+				WHERE dsd.fk_decision = d.id
+			) reasons ON true
+			WHERE a.action_type = $1
+				AND a.datetime = $2
+			ORDER BY a.datetime DESC;`,
+				[actionType, dateStart]
+			);
+			return response.rows;
+		} else {
+			const response = await query(
+				`SELECT 
+					json_build_object(
+						'reasons_array', COALESCE(reasons.reasons_array, '[]'::json),
+						'confidence_score', ROUND(d.confidence_score::numeric, 2),
+						'reasoning_summary', d.reasoning_summary,
+						'action', a.action_type
+					) AS result
+				FROM actions a
+				JOIN prices p ON a.fk_price = p.id
+				JOIN decisions d ON a.fk_decision = d.id
+				LEFT JOIN LATERAL (
+					SELECT json_agg(json_build_object(
+						'id', dsd.fk_sentiment_data::text,
+						'reasoning', dsd.reasoning
+					)) AS reasons_array
+					FROM decisions_sentiment_data dsd
+					WHERE dsd.fk_decision = d.id
+				) reasons ON true
+				WHERE a.action_type = $1
+				ORDER BY a.datetime DESC;`,
+				[actionType]
+			);
+			return response.rows;
+		}
+	} catch (error) {
+		throw new ReturnError(error, error.status);
+	}
+};
+
 export const handlePostAllSentimentRequest = async (date) => {
 	try {
 		const { rows: companies } = await query("SELECT * FROM companies");
