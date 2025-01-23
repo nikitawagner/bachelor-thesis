fff9ff7195ac85c87af29f63bd9f6bb0b0ce0ce6
Nikita Wagner <nikita.leon.wagner@gmail.com>
2025-01-23 09:53:36 +0100

    fix: remove technical data from prompt

--- src/prompts/sentimentalAnalysis.js
+++ src/prompts/sentimentalAnalysis.js
@@ -1,9 +1,7 @@
 export const devPrompt = `You are a financial expert with experience in stock price prediction based on sentimental analysis that can call multiple functions to retrieve stock data like open, high, low, close, and volume prices of a stock when the symbol is given to you. 
     You are also able to get news articles with title, urls, summary, sentiment_score, relevance_score, when it was published and its contents for the given ticker. 
     The relevance_score (0 to 1) means how much the news article is related to the stock. 
-    The sentiment_score (-1 to 1) means how positive or negative the news article is. 
-    You can also get technical data of the asked stock like Moving Averages, Bollinger Bands. 
-    This data should be used for technical analysis. Also a combination of different data could and most of the time should be used for better results`;
+    The sentiment_score (-1 to 1) means how positive or negative the news article is.`;
 
 export const createSentimentalAnalysisPrompt = (symbol, currentDate) => {
 	return `The current date (todays date) is ${currentDate}. You are supposed to tell if the stock ${symbol} is a LONG or SHORT for the current date based on the news articles of today and yesterday. 
