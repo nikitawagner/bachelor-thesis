1dc9a2a6670033aa70dc304603b57d7928af8a95
Nikita Wagner <nikita.leon.wagner@gmail.com>
2025-02-16 13:43:31 +0100

    refactor: remove compare trade data test function from server.js

--- src/server.js
+++ src/server.js
@@ -4,12 +4,6 @@ import cors from "cors";
 import morgan from "morgan";
 import router from "./routes/index.js";
 import errorHandler from "./helper/errorHandler.js";
-import one from "../outputs/comparisons/reproducibility/1.json" assert { type: "json" };
-import two from "../outputs/comparisons/reproducibility/2.json" assert { type: "json" };
-import three from "../outputs/comparisons/reproducibility/3.json" assert { type: "json" };
-import four from "../outputs/comparisons/reproducibility/4.json" assert { type: "json" };
-import five from "../outputs/comparisons/reproducibility/5.json" assert { type: "json" };
-import { compareTradeData } from "./helper/reproducibility.js";
 const app = express();
 dotenv.config();
 cors();
@@ -18,7 +12,6 @@ app.use(morgan("dev"));
 app.use("/v1", router);
 app.use(errorHandler);
 
-compareTradeData([one, two, three, four, five]);
 app.listen(process.env.PORT || 3000, () => {
 	return console.log(
 		`Express is listening at http://localhost:${process.env.PORT || 3000}`
